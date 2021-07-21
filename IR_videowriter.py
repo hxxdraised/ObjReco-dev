e83f57434dcec1b12b09a9b80a66ba6038f6a4ec
Danil <dfarsal@ya.ru>
2021-07-21 18:27:00 +0300

    govnocode fixing

--- IR_videowriter.py
+++ IR_videowriter.py
@@ -1,23 +1,31 @@
 import cv2
+import numpy
 
-link = ''
+link = '/home/youngdanon/Документы/cv/test/bud_bottle.mp4'
 
 cap = cv2.VideoCapture(link)
 
 ret, frame = cap.read()
-h, w = frame.shape
+h, w, _ = frame.shape
 writer = cv2.VideoWriter(
-    '/video/output.avi',
-    cv2.VideoWriter_fourcc(*'MJPG'),  # codec
+    'output.mp4',
+    cv2.VideoWriter_fourcc(*'mp4v'),  # codec
     60,  # fps
     (w, h),  # width, height
     isColor=True)
 
 while True:
-    ret, frame = cap.read()
-    if not ret and frame.mean(axis=0).mean(axis=0)[0] < 10:
+    key = cv2.waitKey(1) & 0xFF
+    ok, frame = cap.read()
+    if not ok or key == ord("q"):
+        print('govno')
         break
+
+    cv2.imshow('wwer', frame)
+
+    if frame.mean(axis=0).mean(axis=0)[0] < 10:
+        continue
     writer.write(frame)
-    
+
 cap.release()
 writer.release()
